4709dc92ed04d3d61a62cf083261423e6d33301d
queenpops <51464806+queenpops@users.noreply.github.com>
2019-06-06 19:10:35 -0400

    Initial Commit

---
+++ closet-js/closet.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+// HAMBURGER NAV
+// $(".close, .hamburger").click(function(e){
+//   e.preventDefault();
+//   $(".main-nav").toggleClass('open-nav');
+//   $("body").toggleClass('nav-opened');
+// });
+
+// SMOOTH SCROLL
+	var scroll = new SmoothScroll('a[href*="#"]');
+// $('.flexslider').flexslider();
+$('.flexslider').flexslider({smoothHeight: true, animation: "slide"});
+  });
